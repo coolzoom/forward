dc371352450b473efca054cb026a120dd14bceff
zz <zz@test.com>
2021-01-08 12:33:26 +0800

    adding webtest

--- src/Forward.Client/Program.cs
+++ src/Forward.Client/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Forward.Client.Entities;
@@ -15,10 +18,66 @@ namespace Forward.Client
 {
     class Program
     {
+
+        public static Dictionary<string, string> dictULR = new Dictionary<string, string> {
+{"1","http://shop.99114.com"},
+{"2","https://www.70dir.com"},
+{"3","http://www.tianqishi.com"},
+{"4","http://www.seek68.cn"},
+{"5","http://zuopinj.com"},
+{"6","http://www.52yxz.net"},
+{"7","https://www.lxybaike.com"},
+{"8","http://www.wangzhanmulu.net"},
+{"9","http://www.wangluo379.cn"},
+{"10","http://www.20dir.com"},
+{"11","http://www.40dir.com"},
+{"12","http://www.vvdir.com"},
+{"13","http://www.hhdir.com"},
+{"14","http://www.wangzhanmulu.net"},
+{"15","http://www.8mtime.com"},
+{"16","https://www.50dir.com"},
+{"17","http://www.zhiyezhuangf.com"},
+{"18","http://www.mhwz.cn"},
+{"19","http://www.xianbaowu.cn"},
+{"20","http://www.shuyi99.com"},
+{"21","http://www.mfisp.com"},
+{"22","http://www.ziyuanmaow.com"},
+{"23","http://ip.tianqishi.com"},
+{"24","https://www.chaxunle.cn"},
+{"25","http://www.oxgood.com"},
+{"26","http://www.huanlj.com"},
+{"27","http://www.digg58.com"},
+{"28","http://www.iapolo.com"},
+{"29","http://www.at"},
+{"30","https://www.wangzhanchi.com"},
+{"31","https://www.wangzhanchi.com"},
+{"32","https://www.wangzhanchi.com"},
+{"33","https://www.wangzhanchi.com"},
+{"34","https://www.wangzhanchi.com"},
+{"35","https://www.wangzhanchi.com"},
+{"36","https://hm.baidu.com"},
+{"37","https://zz.bdstatic.com"},
+{"38","http://push.zhanzhang.baidu.com"},
+{"39","http://guowai.wangzhanchi.com"},
+{"40","http://hk.wangzhanchi.com"},
+{"41","http://www.beian.miit.gov.cn"},
+
+        };
+
         static async Task Main(string[] args)
         {
+            foreach (string u in dictULR.Values)
+            {
+
+                accessweb ab = new accessweb { WEBURL = u };
+                ab.InitializeBackgroundWorker();
+                ab.BGWorkerStart();
+
+            }
+
             var _logger = LogManager.GetCurrentClassLogger();
             _logger.Warn("端口转发程序启动");
+
             try
             {
                 using (var services = Configure())
---
+++ src/Forward.Client/accessweb.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text;
+
+namespace Forward.Client
+{
+    class accessweb
+    {
+        public string WEBURL;
+        private static BackgroundWorker bgWorker = new BackgroundWorker();
+        public  void InitializeBackgroundWorker()
+        {
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.WorkerSupportsCancellation = true;
+            bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+            bgWorker.ProgressChanged += new ProgressChangedEventHandler(bgWorker_ProgessChanged);
+            bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_WorkerCompleted);
+        }
+        public  void BGWorkerStart()
+        {
+            if (bgWorker.IsBusy)
+                return;
+            //this.progressBar1.Maximum = 100;
+            //this.btnStart.Enabled = false;
+            //this.btnStop.Enabled = true;
+            bgWorker.RunWorkerAsync("hello");
+        }
+
+        public  void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (bgWorker.CancellationPending)
+            //    {
+            //        e.Cancel = true;
+            //        return;
+            //    }
+            //    else
+            //    {
+            //        bgWorker.ReportProgress(i, "Working");
+            //        System.Threading.Thread.Sleep(10);
+            //    }
+            //}
+
+            GoTo(WEBURL);
+
+
+        }
+        public  void GoTo(string PageUrl)
+        {
+            try
+            {
+                //string PageUrl = "http://www.baidu.com"; //需要获取源代码的网页
+                WebRequest request = WebRequest.Create(PageUrl); //WebRequest.Create方法，返回WebRequest的子类HttpWebRequest
+                WebResponse response = request.GetResponse(); //WebRequest.GetResponse方法，返回对 Internet 请求的响应
+                                                              //Stream resStream = response.GetResponseStream(); //WebResponse.GetResponseStream 方法，从 Internet 资源返回数据流。
+                                                              //Encoding enc = Encoding.GetEncoding("GB2312"); // 如果是乱码就改成 utf-8 / GB2312
+                                                              //StreamReader sr = new StreamReader(resStream, enc); //命名空间:System.IO。 StreamReader 类实现一个 TextReader (TextReader类，表示可读取连续字符系列的读取器)，使其以一种特定的编码从字节流中读取字符。
+                                                              //ContentHtml.Text = sr.ReadToEnd(); //输出(HTML代码)，ContentHtml为Multiline模式的TextBox控件
+                                                              //resStream.Close();
+                                                              //sr.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+        public  void bgWorker_ProgessChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //string state = (string)e.UserState;//接收ReportProgress方法传递过来的userState
+            //this.progressBar1.Value = e.ProgressPercentage;
+            //this.label1.Text = "处理进度:" + Convert.ToString(e.ProgressPercentage) + "%";
+        }
+
+        public  void bgWorker_WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //if (e.Error != null)
+            //{
+            //    MessageBox.Show(e.Error.ToString());
+            //    return;
+            //}
+            //if (!e.Cancelled)
+            //    this.label1.Text = "处理完毕!";
+            //else
+            //    this.label1.Text = "处理终止!";
+        }
+
+        private  void bgStop()
+        {
+            bgWorker.CancelAsync();
+        }
+    }
+}
